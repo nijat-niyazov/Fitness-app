893ff7d428de07d5a6977cdbe003a5415ba74334
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-03-17 10:30:38 +0400

    exporter issue

--- src/components/exporter.js
+++ src/components/exporter.js
@@ -1,8 +1,13 @@
-import BreadCrumbs from './breadCrumbs/BreadCrumbs';
-import FadingUp from './fadingUp/FadingUp';
+import { lazy } from 'react';
+
 import Footer from './footer/Footer';
 import Navbar from './navbar/Navbar';
 import ProgressBar from './progressBar/ProgressBar';
 import ScrollTop from './scroll-top/ScrollTop';
+
+//  =========== Lazy Components =================
+
+let BreadCrumbs = lazy(() => import('./breadCrumbs/BreadCrumbs'));
+let FadingUp = lazy(() => import('./fadingUp/FadingUp'));
 
 export { BreadCrumbs, Navbar, Footer, ScrollTop, ProgressBar, FadingUp };
