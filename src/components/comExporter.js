db80bf35244061c4983b131cd54f7bcda7539842
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-04-20 19:58:19 +0400

    folder structure

--- src/components/comExporter.js
+++ src/components/comExporter.js
@@ -1,9 +1,10 @@
 import UpperLinks from '../features/breadcrumbs/Links';
-import Footer from './footer/Footer';
-import Navbar from './navbar/Navbar';
 import ProgressBar from '../features/progressBar/ProgressBar';
 import ScrollTop from '../features/scroll-top/ScrollTop';
+import Footer from './footer/Footer';
+import HelmetComponent from './meta-tags/Helmet';
+import Navbar from './navbar/Navbar';
 
 //  =========== Lazy Components =================
 
-export { Navbar, Footer, ScrollTop, ProgressBar, UpperLinks };
+export { Navbar, HelmetComponent, Footer, ScrollTop, ProgressBar, UpperLinks };
---
+++ src/features/featureExporter.js
@@ -0,0 +1,7 @@
+import UpperLinks from './breadcrumbs/Links';
+import ModalComponent from './modal/Modal';
+import ProgressBar from './progressBar/ProgressBar';
+import Highlight from './regexp-highlight/Highlight';
+import ScrollTop from './scroll-top/ScrollTop';
+
+export { UpperLinks, ModalComponent, ProgressBar, Highlight, ScrollTop };
